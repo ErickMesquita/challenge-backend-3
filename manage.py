98a75c041098765a35b70e4125533e09345d3f36
Erick Gabriel Brunoro Mesquita <erick.mesquita@usp.br>
2022-04-12 09:54:07 -0300

    JSON de Configuração mais enxutos

--- manage.py
+++ manage.py
@@ -27,7 +27,7 @@ with open(os.path.join("config", config_json_filename)) as f:
 	config = json.load(f)
 
 # Convert the config into a usable Python dictionary
-config = dict((i["name"], i["value"]) for i in config)
+# config = dict((i["name"], i["value"]) for i in config)
 
 for key, value in config.items():
 	setenv(key, value)
@@ -41,7 +41,13 @@ def cli():
 @cli.command(context_settings={"ignore_unknown_options": True})
 @click.argument("subcommand", nargs=-1, type=click.Path())
 def flask(subcommand):
-	cmdline = ["flask"] + list(subcommand)
+	subcommand_list = list(subcommand)
+
+	if "--host" not in subcommand_list:
+		subcommand_list.append("--host")
+		subcommand_list.append("::1")
+
+	cmdline = ["flask"] + subcommand_list
 
 	try:
 		p = subprocess.Popen(cmdline)
