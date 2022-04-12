28e41f457733e88bca36fb8f9fd3969551ab9ef2
Erick Gabriel Brunoro Mesquita <erick.mesquita@usp.br>
2022-04-12 14:35:49 -0300

    Preparando para poder rodar Testes

--- manage.py
+++ manage.py
@@ -7,11 +7,9 @@ import os
 import json
 import signal
 import subprocess
+import time
 
 import click
-
-docker_compose_file = "docker/development.yml"
-docker_compose_cmdline = ["docker-compose", "-f", docker_compose_file]
 
 
 # Garante que uma variável de ambiente existe e tem um valor
@@ -21,16 +19,18 @@ def setenv(variable, default):
 
 setenv("APPLICATION_CONFIG", default="development")
 
+
 # Lê a configuração do arquivo JSON
-config_json_filename = os.getenv("APPLICATION_CONFIG") + ".json"
-with open(os.path.join("config", config_json_filename)) as f:
-	config = json.load(f)
+def configure_app(config: str):
+	config_json_filename = config + ".json"
+	with open(os.path.join("config", config_json_filename)) as f:
+		config = json.load(f)
 
-# Convert the config into a usable Python dictionary
-# config = dict((i["name"], i["value"]) for i in config)
+	# Convert the config into a usable Python dictionary
+	# config = dict((i["name"], i["value"]) for i in config)
 
-for key, value in config.items():
-	setenv(key, value)
+	for key, value in config.items():
+		setenv(key, value)
 
 
 @click.group()
@@ -57,10 +57,28 @@ def flask(subcommand):
 		p.wait()
 
 
+def docker_compose_cmdline(config: str):
+	configure_app(config)
+	docker_compose_file = os.path.join("docker", f"{config}.yml")
+
+	if not os.path.isfile(docker_compose_file):
+		docker_compose_file2 = os.path.join("docker", f"{config}.yaml")
+		if not os.path.isfile(docker_compose_file2):
+			raise ValueError(f"The file {docker_compose_file} does not exist")
+
+	return [
+		"docker-compose",
+		"-p",
+		config,
+		"-f",
+		docker_compose_file,
+	]
+
+
 @cli.command(context_settings={"ignore_unknown_options": True})
 @click.argument("subcommand", nargs=-1, type=click.Path())
 def compose(subcommand):
-	cmdline = docker_compose_cmdline + list(subcommand)
+	cmdline = docker_compose_cmdline(os.getenv("APPLICATION_CONFIG")) + list(subcommand)
 
 	try:
 		p = subprocess.Popen(cmdline)
@@ -68,6 +86,29 @@ def compose(subcommand):
 	except KeyboardInterrupt:
 		p.send_signal(signal.SIGINT)
 		p.wait()
+
+
+@cli.command()
+@click.argument("filenames", nargs=-1)
+def test(filenames):
+	os.environ["APPLICATION_CONFIG"] = "testing"
+	configure_app(os.getenv("APPLICATION_CONFIG"))
+
+	cmdline = docker_compose_cmdline(os.getenv("APPLICATION_CONFIG")) + ["up", "-d"]
+	subprocess.call(cmdline)
+
+	cmdline = docker_compose_cmdline(os.getenv("APPLICATION_CONFIG")) + ["logs", "db"]
+	logs = subprocess.check_output(cmdline)
+	while "ready to accept connections" not in logs.decode("utf-8"):
+		time.sleep(0.1)
+		logs = subprocess.check_output(cmdline)
+
+	cmdline = ["pytest", "-svv", "--cov=application", "--cov-report=term-missing"]
+	cmdline.extend(filenames)
+	subprocess.call(cmdline)
+
+	cmdline = docker_compose_cmdline(os.getenv("APPLICATION_CONFIG")) + ["down"]
+	subprocess.call(cmdline)
 
 
 @cli.command(context_settings={"ignore_unknown_options": True})
